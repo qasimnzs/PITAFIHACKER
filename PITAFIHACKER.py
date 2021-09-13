04099013c1c09b59d4a4d101e40c15314eb8c71e
qasimnzs <85975321+qasimnzs@users.noreply.github.com>
2021-09-13 14:44:27 +0500

    Update PITAFIHACKER.py
    PITAFI.py

--- PITAFIHACKER.py
+++ PITAFIHACKER.py
@@ -1,49 +1,13 @@
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-reload(sys)
-sys.setdefaultencoding('utf8')
- 
-
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; Android 8.1.0; Chrome/79.0.3945.116) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.116 Mobile Safari/537.36')]
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-
-
-def keluar():
-	print "\033[1;96m[!] \x1b[1;91mExit"
-	os.sys.exit()
-def acak(b):
-    w = 'ahtdzjc'
-    d = ''
-    for i in x:
-        d += '!'+w[random.randint(0,len(w)-1)]+i
-    return cetak(d)
-def cetak(b):
-    w = 'ahtdzjc'
-    for i in w:
-        j = w.index(i)
-        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
-    x += '\033[0m'
-    x = x.replace('!0','\033[0m')
-    sys.stdout.write(x+'\n')
-def jalan(z):
-	for e in z + '\n':
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		time.sleep(00000.1)
-##### LOGO #####
-logo = """
-   Asif Javed
-   ▄︻̷̿┻̿═━一
-   THE QASIM hacker TRICKER 
-   THE LEGEND Hacker
-   THE GAME CHANGER 
-   Pitafi hackers
-   Pitafi ▄︻̷̿┻̿═━一
-\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mPitafi
-\033[1;96mYouTube \033[1;93m: \033[1;92mPitafi Hacker
+import os,platform
+try:
+    import requests
+except ImportError:
+    print ("Installing Module Requests...")
+    os.system("pip2 install requests")
+bit=platform.architecture()[0]
+if bit=="64bit":
+    import kb
+    kb.police()
+elif bit=="32bit":
+    import kb
+    kb.police()
